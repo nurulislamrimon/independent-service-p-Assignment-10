a72bd1bafd00394cd8bbaf4894d72c2dcf681c48
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-05-20 04:25:57 +0600

    sign up and login done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,18 +2,30 @@ import React, { useState } from 'react';
 import './Login.css'
 import { Button, Form } from 'react-bootstrap';
 import SocialLogin from '../../Utilities/SocialLogin/SocialLogin';
-import { useAuthState, useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import { useAuthState, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
     const [check, setCheck] = useState(false);
     const [logedUser] = useAuthState(auth);
+    const [newError, setNewError] = useState('');
     const navigate = useNavigate();
     const location = useLocation();
+    const [signInWithEmailAndPassword, loading, error] = useSignInWithEmailAndPassword(auth);
+
+
     let from = location.state?.from?.pathname || "/";
     const formSubmit = (e) => {
         e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.newPassword.value;
+        if (password.length < 6) {
+            setNewError("Enter at least 6 digit password")
+            return;
+        }
+        signInWithEmailAndPassword(email, password);
     }
 
     logedUser && navigate(from, { replace: true })
@@ -21,25 +33,31 @@ const Login = () => {
         <div className='form-container mx-auto p-5'>
             <h1 className='text-center'>Log in</h1>
             <Form onSubmit={formSubmit}>
-                <SocialLogin></SocialLogin>
-                <div className="d-flex align-items-center mt-2">
-                    <hr className='w-50' /> <span className='px-2'>or</span> <hr className='w-50' />
-                </div>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control type="email" name='email' placeholder="example@gmail.com" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control type="password" name='newPassword' placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" className={!check ? 'text-danger' : 'text-primary'} onClick={() => setCheck(!check)} />
                 </Form.Group>
+
+                <p className="warning text-center">
+                    {error || newError && error.message || newError}
+                </p>
                 <Button variant="dark" type="submit" className='d-block mx-auto' disabled={!check}>
                     Submit
                 </Button>
+                <div className="d-flex align-items-center mt-2">
+                    <hr className='w-50' /> <span className='px-2'>or</span> <hr className='w-50' />
+                </div>
+                <SocialLogin></SocialLogin>
+                <p className='text-center mt-3'>You don't have a account! <span className='toggle-link' onClick={() => navigate('/signup')}>Sign up</span></p>
             </Form>
         </div>
     );
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,47 +1,77 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import SocialLogin from '../../Utilities/SocialLogin/SocialLogin';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useCreateUserWithEmailAndPassword, useSendEmailVerification, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Signup = () => {
     const [check, setCheck] = useState(false);
-    const [user] = useAuthState(auth);
+    const [newError, setNewError] = useState('');
+    const [logedUser] = useAuthState(auth);
     const navigate = useNavigate();
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
-    const formSubmit = (e) => {
+    const [createUserWithEmailAndPassword, user, loading, error] = useCreateUserWithEmailAndPassword(auth);
+    const [sendEmailVerification, sending, verificationError] = useSendEmailVerification(auth);
+    const [updateProfile, updating] = useUpdateProfile();
+
+
+    const formSubmit = async (e) => {
         e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const newPassword = e.target.newPassword.value;
+        const reTypePassword = e.target.reTypePassword.value;
+        if (newPassword !== reTypePassword) {
+            setNewError("Both password didn't matched")
+            return;
+        }
+        if (newPassword.length < 6) {
+            setNewError("Enter at least 6 digit password")
+            return;
+        }
+        await createUserWithEmailAndPassword(email, newPassword)
+        await sendEmailVerification();
+        await updateProfile({ displayName: name })
     }
-    user && navigate(from, { replace: true })
+    logedUser && navigate(from, { replace: true })
     return (
         <div className='form-container mx-auto p-5'>
             <h1 className='text-center'>Sign up</h1>
             <Form onSubmit={formSubmit}>
-                <SocialLogin></SocialLogin>
-                <div className="d-flex align-items-center mt-2">
-                    <hr className='w-50' /> <span className='px-2'>or</span> <hr className='w-50' />
-                </div>
+
+                <Form.Group className="mb-3" controlId="formBasicUserName">
+                    <Form.Label>User Name</Form.Label>
+                    <Form.Control type="text" name='name' placeholder="Nurul Islam" required />
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required />
+                    <Form.Control type="email" name='email' placeholder="example@gmail.com" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>New Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control type="password" name='newPassword' placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Re-type Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" required />
+                    <Form.Control type="password" name='reTypePassword' placeholder="Password" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" className={!check ? 'text-danger' : 'text-primary'} onClick={() => setCheck(!check)} />
                 </Form.Group>
+                <p className="warning text-center">
+                    {error || newError || verificationError && error.message || newError || verificationError}
+                </p>
                 <Button variant="dark" type="submit" className='d-block mx-auto' disabled={!check}>
                     Submit
                 </Button>
+                <div className="d-flex align-items-center mt-2">
+                    <hr className='w-50' /> <span className='px-2'>or</span> <hr className='w-50' />
+                </div>
+                <SocialLogin></SocialLogin>
+                <p className='text-center mt-3'>Are you a member? <span className='toggle-link' onClick={() => navigate('/login')}>Log in</span></p>
             </Form>
         </div>
     );
