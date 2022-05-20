d2a7e8bd516d525b540b9856bc85c726a03e8d23
Nurul Islam Rimon <nurulislamrimon@gmail.com>
2022-05-20 07:11:44 +0600

    completed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import myPhoto from '../../images/MyPhoto.jpg';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            about
-        </div>
+        <section className='about-container d-flex p-2'>
+            <img src={myPhoto} height='100' width='100' className='rounded-circle' alt="my photo" />
+            <div className='ms-3'>
+                <h1>Nurul Islam Rimon</h1>
+                <p>I'm a student of Honours second year. I love technology. I'll work to make daily life easier.</p>
+            </div>
+        </section>
     );
 };
 
